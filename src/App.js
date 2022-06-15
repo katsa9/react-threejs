c7b4e8c7cc958cc5f83feccbb16f839b456ee1de
katsa9 <kathleen.brand2@gmail.com>
2022-06-16 07:23:41 +1000

    add model to scene

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Suspense } from "react";
 import './App.css';
 
-function App() {
+const Model = () => {
+  const gltf = useLoader(GLTFLoader, "./avatar.glb");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={0.6} />
+    </>
+  );
+};
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas className="Canvas"
+      shadows
+      camera={{position: [10, 0, 80], fov: 45}}>
+        <Suspense fallback={null}>
+          <Model position={[0.025, -0.9, 0]}/>
+          <OrbitControls />
+          <ambientLight intensity={0.5} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
+          {/* <Environment preset="sunset" background /> */}
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
-
-export default App;
